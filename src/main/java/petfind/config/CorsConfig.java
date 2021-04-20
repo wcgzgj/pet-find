a85445b53f10bb288187e2ddb16dfdab529efcea
wcgzgj <zhangjia_good@126.com>
2021-04-20 20:38:35 +0800

    添加跨域支持

---
+++ src/main/java/petfind/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package petfind.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @ClassName CorsConfig
+ * @Description TODO
+ * @Author FARO_Z
+ * @Date 2021/4/20 下午8:33
+ * @Version 1.0
+ **/
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedHeaders(CorsConfiguration.ALL)
+                .allowedMethods(CorsConfiguration.ALL)
+                .allowCredentials(true)
+                .maxAge(3600); // 1小时内不需要再预检（发OPTIONS请求）
+    }
+}
