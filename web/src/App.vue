82c39bbe6647b62b0e0b3c890ebcb0009871978f
wcgzgj <zhangjia_good@126.com>
2021-04-20 21:43:36 +0800

    前端界面初始化

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,30 +1,66 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <el-container direction="vertical">
+    <the-header></the-header>
+    <router-view/>
+    <the-footer></the-footer>
+  </el-container>
 </template>
 
+
+
+<script lang="ts">
+  import { defineComponent} from 'vue';
+  import TheFooter from './components/the-footer.vue'
+  import TheHeader from "./components/the-header.vue";
+
+  export default defineComponent({
+    name: 'App',
+    components: {
+      TheHeader,
+      TheFooter
+    },
+  });
+
+</script>
+
+
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  .the-header, .the-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+    margin: 0px;
   }
-}
+
+
+
+  /*.el-aside {*/
+  /*  background-color: #D3DCE6;*/
+  /*  color: #333;*/
+  /*  text-align: center;*/
+  /*  line-height: 200px;*/
+  /*}*/
+
+  /*.el-main {*/
+  /*  background-color: #E9EEF3;*/
+  /*  color: #333;*/
+  /*  text-align: center;*/
+  /*  line-height: 160px;*/
+  /*}*/
+
+  /*
+	找到html标签、body标签，和挂载的标签
+	都给他们统一设置样式
+  */
+  html,body,#app,.el-container{
+    /*设置内部填充为0，几个布局元素之间没有间距*/
+    padding: 0px;
+    /*外部间距也是如此设置*/
+    margin: 0px;
+    /*统一设置高度为100%*/
+    height: 100%;
+  }
+
 </style>
---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <el-footer style="background-color: #545c64" class="the-footer" >
+        <div style="text-decoration-color: #E9EEF3">
+            <span>溯宠</span>
+            <el-divider direction="vertical"></el-divider>
+            为流浪动物找回他们自己的家
+        </div>
+    </el-footer>
+</template>
+
+<script>
+    export default {
+        name: "the-footer"
+    }
+</script>
+
+<style scoped>
+
+
+</style>
