ed52d7f29a92d7fc8323cec681571b85e710f488
wcgzgj <zhangjia_good@126.com>
2021-04-20 20:31:20 +0800

    生成 Demo 持久层代码

--- doc/all.sql
+++ doc/all.sql
@@ -9,3 +9,14 @@ create table `test` (
 ) engine=innodb default charset=utf8mb4 comment='测试';
 
 insert into `test` (id, name, password) values (1, '测试', 'password');
+
+
+# demo列表
+drop table if exists `demo`;
+create table `demo` (
+                        `id` bigint not null comment 'id',
+                        `name` varchar(50) comment '名称',
+                        primary key (`id`)
+) engine=innodb default charset =utf8mb4 comment '测试';
+
+insert into `demo` (id,name) values(2,'测试2');
