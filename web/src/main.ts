e95f91883038474052be8c6b1064078be1ef90f6
wcgzgj <zhangjia_good@126.com>
2021-04-20 20:57:18 +0800

    引入 axios

--- web/src/main.ts
+++ web/src/main.ts
@@ -4,6 +4,11 @@ import router from './router'
 import store from './store'
 import ElementPlus from 'element-plus';
 import 'element-plus/lib/theme-chalk/index.css';
+import axios from 'axios'
+
+
+// 修改axios的baseUrl，这样axios就不用每次都在请求中填写 VUE_APP_SERVER  了
+axios.defaults.baseURL=process.env.VUE_APP_SERVER;
 
 const app = createApp(App)
 app
